2182086d59f8290c75e2e9cad20d8a56dcb7ea10
João Gabriel <joaogbmarques@gmail.com>
2025-05-31 11:27:57 -0300

    removendo comentario

--- src/routes/hospedagem/ServicoRoute.js
+++ src/routes/hospedagem/ServicoRoute.js
@@ -2,11 +2,9 @@ const express = require('express');
 const Router = express.Router();
 const ServicoController = require('../../controllers/hospedagem/ServicoController.js');
 
-// Rotas específicas para serviços de uma hospedagem
 Router.get('/hospedagens/:idHospedagem/servicos', ServicoController.listarServicosPorHospedagem);
 Router.post('/hospedagens/:idHospedagem/servicos', ServicoController.adicionarServicoAHospedagem);
 
-// Rotas para operações gerais de serviços
 Router.put('/servicos/:idServico', ServicoController.atualizarServico);
 Router.delete('/servicos/:idServico', ServicoController.removerServico);
 
