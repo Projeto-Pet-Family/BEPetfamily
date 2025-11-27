b13f61828804f0a2ea935db41e552b4aa455ab60
João Gabriel <joaogbmarques@gmail.com>
2025-11-27 20:28:40 -0300

    fazendo implementacao do metodo de login para hospedagem

--- src/routes/hospedagem/HospedagemRoute.js
+++ src/routes/hospedagem/HospedagemRoute.js
@@ -2,10 +2,15 @@ const express = require('express')
 const Router = express.Router()
 const HospedagemController = require('../../controllers/hospedagem/HospedagemController.js')
 
+// Rotas CRUD para hospedagens
 Router.get('/hospedagens', HospedagemController.lerHospedagens);
 Router.get('/hospedagens/:idHospedagem', HospedagemController.buscarHospedagemPorId);
 Router.post('/hospedagens', HospedagemController.criarHospedagem);
 Router.put('/hospedagens/:idHospedagem', HospedagemController.atualizarHospedagem);
 Router.delete('/hospedagens/:idHospedagem', HospedagemController.excluirHospedagem);
+
+// Rotas de autenticação
+Router.post('/hospedagens/login', HospedagemController.loginHospedagem);
+Router.put('/hospedagens/:idHospedagem/senha', HospedagemController.alterarSenha);
 
 module.exports = Router
